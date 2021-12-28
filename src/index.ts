33f3c33308c0a1d4e76db6cd80c5e232728acd67
GenerelSchwerz <xXSG.KingXx@gmail.com>
2021-12-28 13:55:17 -0500

    updated?

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export {ShotFactory} from "./shotFactory"
 export {StaticShot} from "./staticShot"
 export {AABBComponents, BasicShotInfo, } from "./types"
 export {projectileGravity, trajectoryInfo} from "./calc/constants"
+export {InterceptFunctions} from "@nxg-org/mineflayer-util-plugin" //I feel like this is bad behavior?
